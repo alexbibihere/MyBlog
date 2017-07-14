318397a2eb0144acfbdc04770ffbbba4207d50fb
dingjun133 <1023837465@qq.com>
2017-07-14 18:07:44 +0800

    修复 redis jedisClent 找不到问题 当项目中 jedisClent注入的时候  在redis.xml中 把注释去掉即可

---
+++ MyBlog-Manager-Controller/src/main/resources/SQL/article.sql
@@ -0,0 +1,16 @@
+create TABLE article(
+id int PRIMARY key auto_increment,
+title VARCHAR(100) COMMENT "标题",
+keyword VARCHAR(100) COMMENT "关键字",
+article_describe VARCHAR(500) COMMENT "描述",
+article_column VARCHAR(100) COMMENT "栏目",
+label VARCHAR(100) COMMENT "标签",
+title_images VARCHAR(500) COMMENT "标题图片",
+userName VARCHAR(50) COMMENT "发布用户",
+is_del int DEFAULT '0' COMMENT "是否删除",
+is_release int DEFAULT '0' COMMENT "发布状态",
+is_open int DEFAULT '0' COMMENT "是否公开",
+release_Date TIME COMMENT "发布时间"
+)COMMENT "文章表";
+
+
---
+++ MyBlog-Manager-Dao/src/main/java/com/blog/dao/ArticleMapper.kt
@@ -0,0 +1,10 @@
+package com.blog.dao
+
+/**
+ * Created by Administrator on 2017/7/14.
+ */
+interface ArticleMapper {
+
+
+
+}
